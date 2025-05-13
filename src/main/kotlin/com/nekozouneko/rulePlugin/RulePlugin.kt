5483968b7e90c30b5bce2ac63acbdbe37cbb4de6
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-05-13 14:41:38 +0900

    Initial

---
+++ src/main/kotlin/com/nekozouneko/rulePlugin/RulePlugin.kt
@@ -0,0 +1,14 @@
+package com.nekozouneko.rulePlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RulePlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
